6dce8b490d85bb291d91638979da10271aeb8427
miseda <misedajoseph@gmail.com>
2025-01-07 18:05:55 +0300

    push filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,15 @@
-const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-const imageShortcode = require('./src/_11ty/shortcodes/image-shortcode');
-const markdownLibrary = require('./src/_11ty/libraries/markdown-library');
-const minifyHtml = require('./src/_11ty/utils/minify-html');
-const markdownFilter = require('./src/_11ty/filters/markdown-filter');
-const svgFilter = require('./src/_11ty/filters/svg-filter');
-const browserSyncConfig = require('./src/_11ty/utils/browser-sync-config');
-const { readableDateFilter, machineDateFilter } = require('./src/_11ty/filters/date-filters');
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const imageShortcode = require("./src/_11ty/shortcodes/image-shortcode");
+const markdownLibrary = require("./src/_11ty/libraries/markdown-library");
+const minifyHtml = require("./src/_11ty/utils/minify-html");
+const markdownFilter = require("./src/_11ty/filters/markdown-filter");
+const svgFilter = require("./src/_11ty/filters/svg-filter");
+const browserSyncConfig = require("./src/_11ty/utils/browser-sync-config");
+const {
+  readableDateFilter,
+  machineDateFilter,
+} = require("./src/_11ty/filters/date-filters");
 
 module.exports = function (eleventyConfig) {
   // Plugins
@@ -14,43 +17,46 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
 
   // Filters
-  eleventyConfig.addFilter('markdown', markdownFilter);
-  eleventyConfig.addFilter('readableDate', readableDateFilter);
-  eleventyConfig.addFilter('machineDate', machineDateFilter);
-  eleventyConfig.addFilter('svg', svgFilter);
+  eleventyConfig.addFilter("markdown", markdownFilter);
+  eleventyConfig.addFilter("readableDate", readableDateFilter);
+  eleventyConfig.addFilter("machineDate", machineDateFilter);
+  eleventyConfig.addFilter("svg", svgFilter);
+  eleventyConfig.addFilter("resolvePath", function (path) {
+    return path.startsWith("/") ? path.slice(1) : path;
+  });
 
   // Shortcodes
-  eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
 
   // Libraries
-  eleventyConfig.setLibrary('md', markdownLibrary);
+  eleventyConfig.setLibrary("md", markdownLibrary);
 
   // Merge data instead of overriding
   eleventyConfig.setDataDeepMerge(true);
 
   // Trigger a build when files in this directory change
-  eleventyConfig.addWatchTarget('./src/assets/scss/');
+  eleventyConfig.addWatchTarget("./src/assets/scss/");
 
   // Minify HTML output
-  eleventyConfig.addTransform('htmlmin', minifyHtml);
+  eleventyConfig.addTransform("htmlmin", minifyHtml);
 
   // Don't process folders with static assets
-  eleventyConfig.addPassthroughCopy('./src/favicon.ico');
-  eleventyConfig.addPassthroughCopy('./src/admin');
-  eleventyConfig.addPassthroughCopy('./src/assets/img');
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("./src/assets/img");
 
   // Allow Turbolinks to work in development mode
   eleventyConfig.setBrowserSyncConfig(browserSyncConfig);
 
   return {
-    templateFormats: ['md', 'njk', 'html'],
-    markdownTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
     passthroughFileCopy: true,
     dir: {
-      input: 'src',
-      layouts: "_layouts"
+      input: "src",
+      layouts: "_layouts",
     },
   };
 };
